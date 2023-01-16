6d4ce89a8772e1d39cb69f3464377e1236b5a2dd
qoalstn44 <qoalstn44@naver.com>
2023-01-16 11:45:59 +0900

    기본 타입

---
+++ index.ts
@@ -0,0 +1,49 @@
+// //string
+// let str: string = "TS";
+
+// str = 123
+// str = true
+// str = undefined
+
+// let fullName: string = 'NBC';
+// let age : number = 30;
+// let sentenece : string = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next month.`;
+
+// //Number
+
+// let decimal: number = 6;
+// let hex: number = 0xf00d;// 0x
+// let binary: number = 0b1010;//0b
+// let octal: number = 0o744;//0o
+
+// //Boolean
+
+// let isTrue: boolean = true;
+// let isFalse: boolean = false;
+
+// //ts boolean 값이고 참/거짓
+
+// //Array
+
+// let arr : number[] = [1,2,3];
+// let arr1 : Array<number> = [1,2,3];
+// let arr2 : string[] = ['a','b','c'];
+// let arr3 :boolean[] = [true,false,true];
+
+// //Tuple
+// let x: [string, number];
+// x = ["hi", 10];
+// // x= [10, "hi"]; //Error
+// console.log(x[0], x[1]);
+
+// let Tuple: [string, number, boolean] = ["hi", 10, true]; //이렇게 길게 쓸수있음
+
+// //any 어떤이든 타입없이 가능하다. (자바스크립트와 같음)
+// let str: any = "hi";
+
+// str = 123;
+// str = true;
+// str = undefined;
+// str = "abc";
+// let num: any = 123;
+// let any: any = ["a", 2, true];
