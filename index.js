51668dbc6077990a968ab97363c9109097389bbf
qoalstn44 <qoalstn44@naver.com>
2023-01-16 17:19:18 +0900

    함수 타입, Eunm타입 정리

---
+++ index.js
@@ -0,0 +1,14 @@
+// function add (n1, n2) {
+//     return n1 + n2;
+// } //index.js 함수타입 Type Assertion (타입 단언) 예시
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+// function add(n1, n2) {
+//   if (!n2) return n1;
+//   return n1 + n2;
+// } //index.js 함수타입 printResult 예시
+
+// const a2 = add2(2, 3); // 5
+// const b2 = add2(2); // 2
+// const c2 = add2(2, 3, 4, 5); // 5
--- index.ts
+++ index.ts
@@ -9,12 +9,16 @@
 // let age : number = 30;
 // let sentenece : string = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next month.`;
 
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 // //Number
 
 // let decimal: number = 6;
 // let hex: number = 0xf00d;// 0x
 // let binary: number = 0b1010;//0b
 // let octal: number = 0o744;//0o
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // //Boolean
 
@@ -23,12 +27,16 @@
 
 // //ts boolean 값이고 참/거짓
 
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 // //Array
 
 // let arr : number[] = [1,2,3];
 // let arr1 : Array<number> = [1,2,3];
 // let arr2 : string[] = ['a','b','c'];
 // let arr3 :boolean[] = [true,false,true];
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // //Tuple
 // let x: [string, number];
@@ -38,6 +46,8 @@
 
 // let Tuple: [string, number, boolean] = ["hi", 10, true]; //이렇게 길게 쓸수있음
 
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 // //any 어떤이든 타입없이 가능하다. (자바스크립트와 같음)
 // let str: any = "hi";
 
@@ -46,7 +56,10 @@
 // str = undefined;
 // str = "abc";
 // let num: any = 123;
+
 // let any: any = ["a", 2, true];
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // //void 함수가 아무것도 리턴하지 않을때 사용
 
@@ -61,6 +74,8 @@
 
 // let unionType: number | null = 10;
 
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 // //never (함수가 절대 리턴하지 않을때 사용)
 
 // function a(): never { // 함수 a는 무한루프를 돌고있다.
@@ -72,4 +87,87 @@
 //   throw new Error("error");
 // }
 
-// //object (자바스크립트와 같음)
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+// //Type Assertion (타입 단언)
+// function add(n1: number, n2: number): number { //n1, n2는 number타입이다. : number는 생략 할수있다.
+//   return n1 + n2;
+// }
+
+// const add = (n1: number, n2: number): number => { //화살표 함수예시
+//   return n1 + n2;
+// };
+
+// const a = add(1, 2);
+// const b = add("1", 2); //에러 '1'은 number타입이 아니다.
+// const c = add(10, 20, 30); //에러 3개의 인자를 받는데 2개만 받았다. 30은 3개의 인자다 그럼으로 무시된다.
+// const d = add(10); //에러 2개의 인자를 받는데 1개만 받았다. 10은 1개의 인자다 그럼으로 무시된다.
+
+// //반환값이 없을때는 void를 쓴다.
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+
+// printResult(5)
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+//optional parameter (선택적 매개변수)
+
+// function add2(n1: number, n2?: number, n3?: number): number {
+//   if (!n2) return n1;
+//   return n1 + n2;
+// }
+
+// const a2 = add2(1, 2); // 5
+// const b2 = add2(10, 20, 30); // 2
+// const c2 = add2(10); // 5
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+// 숫자형 이넘
+
+// enum Direction {
+//   Up = 1,
+//   Down,
+//   Left,
+//   Right,
+// }
+
+// console.log(Direction.Up, Direction.Down, Direction.Left); // 0
+
+// const up: Direction = Direction.Up;
+// // Direction = Direction.UP | Direction.Down | Direction.Left | Direction.Right
+// const leftOrRight: Direction.Left | Direction.Right = Direction.Left;
+
+// console.log(Direction[2]);
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+//문자형 이넘
+// enum Direction { //위의 숫자형 이넘과 같은 결과를 낸다. 하지만  UP에 숫자를 넣고 실행하면 0, DOWN, LEFT 로 출력하게 된다.
+//   Up = "UP",
+//   Down = "DOWN",
+//   Left = "LEFT",
+//   Right = "RIGHT",
+// }
+
+// console.log(Direction.Up, Direction.Down, Direction.Left); // 0
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// //복합형 이넘 (권고 하지않음)
+// enum BooleanLikeHeterogeneousEnum {
+//     No = 0,
+//     Yes = "YES",
+// }
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
