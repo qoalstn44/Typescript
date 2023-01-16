efde4954cfc9da6b7452886f6f6439e1776b60bd
qoalstn44 <qoalstn44@naver.com>
2023-01-16 10:48:44 +0900

    first commit

---
+++ temp.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello_1 = require("./hello");
+const str = "TS";
+const hi = () => {
+  console.log(`Hi ${str}!`);
+};
+(0, hello_1.helloFunc)();
